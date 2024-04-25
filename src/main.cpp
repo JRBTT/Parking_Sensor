61299d04cc853a32a34671f0de818177e964b908
JRBTT <jason.ray.buntting@gmail.com>
2024-04-25 21:18:04 +1000

    additional comments

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ ISR(TIMER0_OVF_vect)
 {
   num0V++;
   if (num0V >= delay){
-    toggle = !toggle;
+    toggle = !toggle; // reverses current value if 0 to 1 or 1 to 0 after defined delay.
     num0V = 0;
   }
 }
@@ -91,10 +91,10 @@ void setup()
 {
   usart_init(MYUBRR); // 103-9600 bps; 8-115200
   bitSet(TIMSK0, TOIE0); // enable timer0 overflow interrupt
-  set_tc1_mode(3); //TOP 0x03FF
-  setPrescaler_tc1(2);
-  bitSet(TCCR1A, COM1A1);
-  bitSet(TCCR1A, COM1B1);
+  set_tc1_mode(3); // pwm mode TOP 0x03FF 1023
+  setPrescaler_tc1(2); // timer1 for pwm
+  bitSet(TCCR1A, COM1A1); // clear on compare match
+  bitSet(TCCR1A, COM1B1); // clear on compare match
   bitSet(DDRB, LEDPIN); // set pin 9 as output
   bitSet(DDRB, BUZZERPIN); // set pin 10 as output
   bitSet(DDRD, TRIGGER_PIN); 
@@ -107,17 +107,17 @@ void setup()
   bitSet(ADCSRA, ADEN); // enable ADC
   bitSet(ADCSRA, ADIE); // enable ADC interrupt
   sei(); // enable global interrupts
-  setPrescaler_tc0(5);
+  setPrescaler_tc0(5); // timer0 for just keeping time 1024 prescaler controlling on and off.
 }
 
 int main()
 {
   setup();
-  int state = toggle;
+  int state = toggle;   // 0 = off, 1 = on
   int previous_button1 = 1;
   int previous_button2 = 1;
-  int volume[3] = {5, 400, 800};
-  int brightness[3] = {20, 400, 1023};
+  int volume[3] = {5, 400, 800}; //changed by button 2
+  int brightness[3] = {20, 400, 1023}; //changed by ADC photoresistor
   int i = 0;
   int newOCR1A = MAXPWM;
 
@@ -125,30 +125,33 @@ int main()
   {
     if (adcReady){
       adcReady = false;
+      // threshold for brightness up to 1023.
       if (adcResult > 750) {
-        newOCR1A = brightness[0];
+        newOCR1A = brightness[0]; //torchlight will make it dark
       }
-      else if (adcResult >= 400 && adcResult <= 750) {
+      else if (adcResult >= 400 && adcResult <= 750) { // room light will make it medium
         newOCR1A = brightness[1];
       }
       else{
-        newOCR1A = brightness[2];
+        newOCR1A = brightness[2]; // finger covering the photoresistor will make it bright
       }
       // usart_tx_string("ADC: ");
       // usart_tx_float((float)adcResult,3,2);
       // usart_transmit('\n');
   }
+    // switch device on or off
     int current_button1 = bitRead(PIND, BUTTON1);
     if (current_button1 != previous_button1 && current_button1 == 0) {
         _delay_ms(50);
         if (bitRead(PIND, BUTTON1) == current_button1) {
-            sound = !sound;
+            sound = !sound; // switch device on or off
         }
     }
     previous_button1 = current_button1;
 
 
-    int current_button2 = bitRead(PIND, BUTTON2);
+    // cycle through the volume levels
+    int current_button2 = bitRead(PIND, BUTTON2); 
     if (current_button2 != previous_button2 && current_button2 == 0) {
         _delay_ms(50);
         if (bitRead(PIND, BUTTON1) == current_button1) {
@@ -160,9 +163,12 @@ int main()
     }
     previous_button2 = current_button2;
 
+    
+    // send ultrasound signal and listen for echo
     pulseTrigger();
     float buffer = listen();
     _delay_us(10);
+    // checks if there is a valid result from ultrasonic sensor
     if (buffer != -1){
       usart_tx_string("Distance: ");
       usart_tx_float(buffer, 3, 2);
@@ -187,12 +193,13 @@ int main()
     usart_tx_float((float)delay, 3, 2);
     usart_transmit('\n');
     
+    // if the device switched on/off via button
     if (sound){
       if (state != toggle){
         state = toggle;
         if (state){
-          OCR1A = newOCR1A;
-          OCR1B = volume[i];
+          OCR1A = newOCR1A; // led pwm timer1 register
+          OCR1B = volume[i]; // buzzer pwm timer1 register
           updateADC();
           //bitSet(PORTB, PIN);
         }
@@ -203,6 +210,7 @@ int main()
         }
       }
     }
+    // switched off
     else{
       _delay_ms(50);
       OCR1A = 0;
